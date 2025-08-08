807e0f5a5398d08c58ec2e7f22dbdcb9625b7647
Rory0304 <mitty0304@naver.com>
2025-08-08 20:22:24 +0900

    feat: update main page

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import { MainBanner } from "@/components/pages/main/MainBanner";
+import { MainImageUploader } from "@/components/pages/main/MainImageUploader";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <MainBanner />
+      <MainImageUploader />
+    </div>
+  );
+}
